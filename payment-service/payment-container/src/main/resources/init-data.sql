11f634afd7e7e8c31cbd890d434c3723a5a2968d
mustardpenguin <andyha234@gmail.com>
2024-03-18 23:06:21 -0400

    Updated project to use java 17 when building images, services added to deployment

--- payment-service/payment-container/src/main/resources/init-data.sql
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -3,5 +3,5 @@ INSERT INTO "payment".balances (id, account_id, email, credit) VALUES
 (
     '92ac1a01-86a6-4c7c-b8c9-13094f216272',
     '92ac1a01-86a6-4c7c-b8c9-13094f21627e',
-    'test@gmail.com', 0
+    'test@gmail.com', 50.00
 );
