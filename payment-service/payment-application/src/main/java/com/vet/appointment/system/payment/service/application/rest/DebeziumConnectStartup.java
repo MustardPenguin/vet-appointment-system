cf4e0539e898cefe565bb1135532a501c8ec568f
mustardpenguin <andyha234@gmail.com>
2024-04-08 21:13:57 -0400

    Availability chart added

---
+++ payment-service/payment-application/src/main/java/com/vet/appointment/system/payment/service/application/rest/DebeziumConnectStartup.java
@@ -0,0 +1,35 @@
+package com.vet.appointment.system.availability.service.application.rest;
+
+import com.vet.appointment.system.application.debezium.DebeziumConnectConfig;
+import com.vet.appointment.system.application.debezium.DebeziumConnectService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DebeziumConnectStartup implements ApplicationRunner {
+
+    @Value("${applicationDeploymentName}")
+    private String APPLICATION_DEPLOYMENT_NAME;
+
+    private final DebeziumConnectService debeziumConnectService;
+
+    public DebeziumConnectStartup(DebeziumConnectService debeziumConnectService) {
+        this.debeziumConnectService = debeziumConnectService;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        DebeziumConnectConfig availabilityResponseConfig = DebeziumConnectConfig.builder()
+                .tableIncludeList("availability.appointment_outbox")
+                .slotName("availability_appointment_outbox_slot")
+                .topicPrefix("availability_response")
+                .name(APPLICATION_DEPLOYMENT_NAME + "-appointment-connector")
+                .build();
+
+        debeziumConnectService.createDebeziumConnector(availabilityResponseConfig);
+    }
+}
